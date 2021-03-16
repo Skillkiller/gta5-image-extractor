24479f80f3d0d50dc6ae9bb8c2a7a0e947df5267
Skillkiller <18444809+Skillkiller@users.noreply.github.com>
2021-03-16 23:01:36 +0100

    Clear project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input } from '@angular/core';
-import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -8,57 +7,4 @@ import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 })
 export class AppComponent {
   title = 'gtaimage';
-
-  @Input() name: string | undefined;
-  image: SafeUrl = "";
-
-  constructor(private sanitizer: DomSanitizer) {}
-
-  listFile = [
-    {  }
-  ]
-
-  fileChange(event: Event) {
-    const input = event.target as HTMLInputElement;
-
-    if (!input.files?.length) {
-        return;
-    }
-
-    const file = input.files[0];
-    console.log(file);
-    var r = new FileReader();
-    r.onload = (e) => {
-      let byteArrayFile: Uint8Array = new Uint8Array(e.target?.result as any);
-      let byteArray: Uint8Array = new Uint8Array(byteArrayFile.byteLength)
-      let index: number = 0;
-      const offset: number = 292;
-      for(; offset + index < byteArrayFile.byteLength; index++) {
-        byteArray[index] = byteArrayFile[offset + index];
-
-        if(index >= 3) {
-          if(byteArrayFile[offset + index - 1] == 255 && byteArrayFile[offset + index] == 217) {
-            break;
-          }
-        }
-      }
-
-      
-      byteArray = byteArray.slice(0, index + 1);
-      this.image = this.sanitizer.bypassSecurityTrustUrl('data:image/jpeg;base64,' + _arrayBufferToBase64(byteArray));
-
-    };
-    r.readAsArrayBuffer(input.files[0]);
-  }
-}
-
-function _arrayBufferToBase64( buffer: any ) {
-  console.log(buffer);
-  var binary = '';
-  var bytes = new Uint8Array( buffer );
-  var len = bytes.byteLength;
-  for (var i = 0; i < len; i++) {
-      binary += String.fromCharCode( bytes[ i ] );
-  }
-  return window.btoa( binary );
 }
